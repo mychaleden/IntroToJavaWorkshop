ca72865a56b49739e523c589d162dd0e1168ae20
Mrfuzzy <league@mini222.local>
2016-02-10 17:32:09 -0800

    lit robot

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,19 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot MBot=new Robot();
+		for (int i = 0; i < 4.; i++) {
+	MBot.penDown();
+	MBot.move(100);
+	MBot.turn(90);
+}
+	
+
+
+
+
+
 
 	
-		
-		
-	}
+		}
 }
--- src/day1/robot/Spiral.java
+++ src/day1/robot/Spiral.java
@@ -5,14 +5,20 @@ import org.jointheleague.graphical.robot.Robot;
 public class Spiral {
 
 	public static void main(String[] args) {
-		// 1. Create a new Robot
 		
-		// 5. Set your robot's pen to the down position
+Robot r2d2= new Robot();		
 
-		// 3. Set the robot to go at max speed (10)
-
+r2d2.penDown();
+		
+r2d2.setSpeed(10);
 		// 4. Do the following (steps 7-9) 75 times
-
+for (int i = 0; i < 10000.; i++) {
+	r2d2.setRandomPenColor();
+	r2d2.move(5*i);
+	r2d2.turn(360/16);
+	r2d2.setPenWidth(25);
+	
+}
 			// 7. Change the pen color to random
 	
 			// 6. Move the robot 5 times the current line number you are drawing (5*i)
@@ -24,4 +30,6 @@ public class Spiral {
 			// 9. Set the pen width to i
 
 	}
+	
+			
 }
